02f18028ee2a28ba71c60906a1d47c519c1d0b50
Keshav766 <75837310+Keshav766@users.noreply.github.com>
2026-04-10 12:58:30 +0530

    Feat: added User Model and verified connection

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config("./.env");
+import User from "./models/user.js";
 
 const PORT = process.env.PORT;
 console.log(PORT)
@@ -12,14 +13,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const db = mongoose.connect(DB_URL)
     .then(() => console.log("MongoDB connected"))
     .catch((e) => console.log("MongoDB Error encountered"));
 
-
-app.get("/", async (req, res) => {
-    res.send("this is root");
+app.post("/", async (req, res) => {
+    const { name, email, password } = req.body;
+    const createdUser = await User.create({ name, email, password });
+    res.json({
+        status: "success",
+        data: createdUser,
+    })
 })
 
 app.listen(PORT, () => {
