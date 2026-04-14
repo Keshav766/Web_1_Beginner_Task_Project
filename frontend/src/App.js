adb2d98ab18d60df7fefe55a80470d64b61b37be
Keshav766 <75837310+Keshav766@users.noreply.github.com>
2026-04-14 17:22:55 +0530

    Feat(auth): Implemented protected routes for authenticated access of /profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
 import Profile from "./pages/Profile.jsx";
 import Home from "./pages/Home.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
